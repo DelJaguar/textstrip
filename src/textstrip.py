c677d33212e971493bb7f7dbb16c5daa35ccd935
Julian Aguirre <emailjulianhere@gmail.com>
2021-11-29 20:43:15 -0700

    updated strip to use regex to textstrip.py

--- src/textstrip.py
+++ src/textstrip.py
@@ -2,12 +2,12 @@
 #   Program to strip out special characters from a string  #
 #                   By Julian Aguirre                      #
 #############################################################
-def main(arg):
-    variable = ''
-    for i in arg.split('-'):
-        variable = variable + i
+import re 
 
-    print(variable)
+def main(arg):
+    target_str = arg 
+    result = re.sub(r"\D","",arg)
+    print(result)
     
 
 main("3-3-3-3-3-3-3-3-3-3-3-3")
